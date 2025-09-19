56ded1665020c8f4795e61c713cbc56417e695d1
invortoai <automationindia.inc@gmail.com>
2025-09-19 15:34:35 +0530

    modified:   infra/terraform/modules/jambonz-media/main.tf

--- infra/terraform/modules/jambonz-media/main.tf
+++ infra/terraform/modules/jambonz-media/main.tf
@@ -149,7 +149,7 @@ resource "aws_launch_template" "jambonz" {
     name = aws_iam_instance_profile.jambonz.name
   }
 
-  user_data = base64encode(templatefile("$${path.module}/user-data.sh", {
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
     environment = var.environment
     domain      = var.domain
     redis_url   = var.redis_url
